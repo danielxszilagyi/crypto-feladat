edb7b8b0c35e8d809a7046486b883b43b45735c8
Dániel <szidani1999@gmail.com>
2024-02-28 20:33:08 +0100

    Add max width

--- src/app/components/tabs-view/tab/tab.component.ts
+++ src/app/components/tabs-view/tab/tab.component.ts
@@ -9,11 +9,17 @@ import { ChartComponent } from './components/chart.component';
   standalone: true,
   imports: [CommonModule, MaterialModule, ChartComponent],
   template: `
-    <h3>asset_id_base: {{ data.asset_id_base }}</h3>
-    <h3>symbol_id: {{ data.symbol_id }}</h3>
-    <app-chart [symbolData]="data"></app-chart>
+    <div id="tab">
+      <h3>asset_id_base: {{ data.asset_id_base }}</h3>
+      <h3>symbol_id: {{ data.symbol_id }}</h3>
+      <app-chart [symbolData]="data"></app-chart>
+    </div>
   `,
-  styles: ``,
+  styles: `
+  #tab{
+    max-width: 1800px;
+  }
+  `,
 })
 export class TabComponent {
   @Input() data!: Symbol;
