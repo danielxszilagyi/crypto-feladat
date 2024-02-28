25264e7d5525262bd0cbc8c4496b9120fb10164c
Dániel <szidani1999@gmail.com>
2024-02-28 20:06:26 +0100

    Add development environment configuration

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  baseUrl: 'https://rest.coinapi.io/v1/',
+  options: {
+    headers: {
+      'X-CoinAPI-Key': 'F5344750-F538-435A-B927-340D1EDE10DD',
+    },
+  },
+};
